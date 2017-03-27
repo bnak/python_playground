6be102c1da695d771953bd56da31243d68156445
Beth Nakamura <bnak@umich.edu>
2017-03-26 18:08:05 -0700

    Initial commit with main function

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+
+def main():
+	# Access conf file by config[key] after loading file
+    # config = {}
+    # execfile("./ignore_files/project.conf", config)
+
+
+    
+    print "Main function ran!"
+
+
+if __name__ == "__main__":
+    main()
